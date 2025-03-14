f5088631db3aac831b2595fda4417f88c93db3aa
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-03-14 15:41:49 +0530

    Add files via upload

---
+++ list_comprehension.py
@@ -0,0 +1,48 @@
+
+# words = ['automobile','car','bmw','audi']
+# words = [ word.upper() if word.startswith('a') else word for word in words ]
+# print(words)
+
+# number = [4*x for x in range(1,21)]
+# print(number)
+
+# number = [x*x for x in range(1,16) if x %2 != 0]
+# print(number)
+
+# words = ['helloo','brototype','iphone','hey']
+# word =[x for x in words if len(x)>5]
+# print(word)
+
+# mylist =[(x,x**3) for x in range(1,11) ]
+# print(mylist)
+
+# words = ['helloo','brototype','iphone','hey']
+# len_words =[len(word) for word in words]
+# print(len_words)
+
+# marks = [20,39,99,82,49,100]
+# grade = ['pass' if x>80 else 'fail' for x in marks ]
+# print(grade)
+
+# numbers = ['fizz' if x%3==0 else 'buzz' if x%5==0 else x for x in range(1,51) ]
+# print(numbers)
+
+
+# number = [x for x in range(1,51) if x%2==0 and x%5==0 ]
+# print(number)
+
+# name = 'amil'
+# age = 22
+# print(f"my name is {name} and i am {age} years old")
+
+
+# words = ['hello','hey','welcoem']
+# print(words.count('hello'))
+
+
+
+# list1= [1,2,3,4,5]
+# result = [x*3 if x%2 == 0 else x for x in list1 if x >2]
+# print(result)
+
+
