ba030539ef9e9637369ec2b7be35b3c549dbb2a9
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-04-03 20:02:58 +0530

    Add files via upload

---
+++ OOP using python.py
@@ -0,0 +1,96 @@
+
+# class computer:
+#     def __init__(self,cpu,ram):
+#         self.cpu = cpu
+#         self.ram = ram
+    
+#     def nihal(self):
+#         print('nihal is ',self.cpu)    
+#     def config(name):
+#         print('config is ',name.cpu,name.ram)
+        
+# com1=computer('i7','16gb')
+# com1.config()
+# com1.nihal()
+
+
+# class person:
+#     def __init__(self):
+#         self.name = 'amil'
+#         self.age = '22'
+#         print('i am  ' +self.name)
+       
+# p1=person()
+# p2 =person()
+
+# p1.name = 'jaseel'
+# p1.age = 33
+
+
+
+# -------INNER CLASS-------
+
+# class student:
+#     def __init__(self,name,age):
+#         self.name=name 
+#         self.age= age
+#         self.lap = self.laptop()
+    
+#     def show(self):
+#         print(f"my name is {self.name}, and i am {self.age} years")
+#         self.lap.show()
+        
+#     class laptop:
+#         def __init__(self):
+#             self.brand = 'Dell'
+#             self.model = 'G15'
+            
+#         def show(self):
+#             print(f"my laptop is {self.brand} nd model is {self.model}")
+        
+# s1=student('amil',22)
+# s1.show()
+
+
+# class A:
+#     def feature1(self):
+#         print('featrue one is working')
+#     def feature2(self):
+#         print('feature two also workingg')
+        
+# class B():
+#     def feature3(self):
+#         print('feature three is also working')
+        
+# class C(A,B):
+#     def feature4(self):
+#         print('evrything was working perfectly')
+            
+# c1 =C()
+# c1.feature2()
+
+
+
+
+
+
+
+
+
+
+
+
+# class computer:
+#     def __init__(self,cpu,ram):
+#         self.cpu = cpu
+#         self.ram = ram
+    
+#     def nihal(self):
+#         print('nihal is ',self.cpu)    
+#     def config(name):
+#         print('config is ',name.cpu,name.ram)
+        
+# com1=computer('i7','16gb')
+# com1.config()
+# com1.nihal()
+
