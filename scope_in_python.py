5fd9658f4b253dbba1366fadc62638a9d4cd2aaf
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-04-04 23:22:39 +0530

    Add files via upload

---
+++ scope_in_python.py
@@ -0,0 +1,22 @@
+# -------SCOPE---------
+# def check_scope():
+#     def do_local():
+#         test = 'it is local test'
+#     def do_non_local():
+#         nonlocal test
+#         test = 'it is non local test '
+#     def do_global():
+#         global test
+#         test ='it is gloabal test'
+        
+#     test ='default'
+#     do_local()
+#     print('test value after do local:  '+test)
+#     do_non_local()
+#     print('test value after do_non_local:  '+test)
+#     do_global()
+#     print('test value after do_gloabl:  '+test)
+    
+    
+# check_scope()
+# print('test value after do_global:  ' + test)
