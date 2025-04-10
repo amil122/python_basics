5fe095ee1630435ccf92321b555512096c822673
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-04-10 19:13:11 +0530

    Add files via upload

---
+++ method_overloading.py
@@ -0,0 +1,37 @@
+
+
+# --------METHOD OVERLOADING-------------
+# class student:
+#     def __init__(self,m1,m2):
+#         self.m1 =m1
+#         self.m2= m2
+#     def sum(self):
+#         s2 = self.m1+self.m2
+#         return s2
+        
+# s1=student(11,22,33)
+# print(s1.sum())
+
+
+# class Calculator:
+#     def add(self, *args):
+#         if len(args) == 1:
+#             return args[0]
+#         elif len(args) == 2:
+#             return args[0] + args[1]
+#         elif len(args) == 3:
+#             return args[0] + args[1] + args[2]
+#         else:
+#             raise ValueError("Unsupported number of arguments for add method")
+
+
+# calc = Calculator()
+
+# result1 = calc.add(2)
+# result2 = calc.add(2, 3)
+# result3 = calc.add(2, 3, 4)
+
+# print("Result 1:", result1)
+# print("Result 2:", result2)
+# print("Result 3:", result3)
+
