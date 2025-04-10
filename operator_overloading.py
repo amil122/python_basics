ca7f6e77bb13026d22101c20476fa0825e5c6f7b
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-04-10 19:12:42 +0530

    Add files via upload

---
+++ operator_overloading.py
@@ -0,0 +1,24 @@
+
+
+#   ------OPERATOR OVERLOADING-----
+# class student:
+#     def __init__(self,m1,m2):
+#         self.m1= m1
+#         self.m2 =m2
+    
+#     def __add__(self,other):
+#         m1= self.m1 + other.m1
+#         m2 = self.m2 + other.m2
+#         s3 = student(m1,m2)
+#         return s3
+    
+#     def __str__(self) :
+#         return ("{} {} " .format(self.m1,self.m2))
+   
+#     def show(self):
+#         print(f"mark1 is {self.m1} and mark2 is {self.m2}")    
+# s1= student(25,99)
+# s2 = student(54,33)
+# s3 =s1+s2
+# s3.show()
+# print(s3)
