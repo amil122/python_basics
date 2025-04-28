870e2d7af1cbb9500e668d062f9652c57ed400b4
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-04-28 23:43:41 +0530

    Add files via upload

---
+++ abstract_method.py
@@ -0,0 +1,38 @@
+
+
+# ----------ABSTRACT CLASS---------
+# from abc import ABC,abstractmethod
+
+# class vehicle(ABC):
+#     def open_car(self):
+#         print('the car was opned')
+#     def start_engine(self):
+#         print('the car is started')
+        
+#     @abstractmethod
+#     def gearbox(self):
+#         pass
+    
+# class car(vehicle):
+#     def sunroof(self):
+#         print('youre car have a sunroof')
+        
+#     def gearbox(self):
+#         print('it is a automatic')
+
+# class car1(vehicle):
+#     def sunroof(self):
+#         print('youre car have a sunroof')
+        
+#     def gearbox(self):
+#         print('it is a manual')
+# c= car()
+# c.sunroof()
+# c.gearbox()
+# c.open_car()
+# print()
+# c= car1()
+# c.sunroof()
+# c.gearbox()
+# c.open_car()
+
