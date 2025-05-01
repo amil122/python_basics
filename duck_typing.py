abc31d13e330f71bccd6ec70e04018d527d9dd4b
Muhammed Amil <69601510+amil122@users.noreply.github.com>
2025-05-02 00:18:04 +0530

    Add files via upload

---
+++ duck_typing.py
@@ -0,0 +1,23 @@
+
+#  -------DUCK TYPING---------
+# class laptop:
+#     def code(self,ide):
+#         ide.execute()
+        
+# class pycharm():
+#     def execute(self):
+#         print('it will compile')
+#         print('it will execute after that')
+#         print('it is awsome')
+# class myeditor():
+#     def execute(self):
+#         print('this is my editor')
+#         print('it will compile')
+#         print('it will execute after that')
+#         print('it is awsome')
+        
+        
+        
+# ide = myeditor()
+# c1 = laptop()
+# c1.code(ide)
